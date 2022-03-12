3e9d4ba9853688a7972a12551e634d29327936da
marco <m.solari@mypass.technology>
2022-03-12 22:04:01 +0100

    removing aws-aplify dependency

--- src/components/Authentication.js
+++ src/components/Authentication.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useEffect, useContext } from "react";
-import Amplify/*, { I18n }*/ from "aws-amplify";
+//import Amplify/*, { I18n }*/ from "aws-amplify";
 //import { useTranslation } from "react-i18next";
 import { currentAuthenticatedUser } from "../libs/TrackPromise";
 import { AuthContext } from "../providers/AuthProvider";
@@ -24,20 +24,20 @@ function Authentication() {
      */
     setAuth({isAuthenticated: undefined, user: null});
 
-    window.LOG_LEVEL = (process.env.NODE_ENV !== "production" && config.debugAwsAmplify ? "DEBUG" : "");
+    //window.LOG_LEVEL = (process.env.NODE_ENV !== "production" && config.debugAwsAmplify ? "DEBUG" : "");
 
-    Amplify.configure({
-      Auth: {
-        oauth: {...config.oauth,
-          redirectSignIn: isLocalhost ? config.oauthRedirectSignInLocal : config.oauthRedirectSignInPublic,
-          redirectSignOut: isLocalhost ? config.oauthRedirectSignOutLocal : config.oauthRedirectSignOutPublic,
-        },
-        region: process.env.REACT_APP_REGION,
-        userPoolId: process.env.REACT_APP_USER_POOL_ID,
-        userPoolWebClientId: process.env.REACT_APP_USER_POOL_WEB_CLIENT_ID,
-        storage: localStorage,
-      },
-    });
+    // Amplify.configure({
+    //   Auth: {
+    //     oauth: {...config.oauth,
+    //       redirectSignIn: isLocalhost ? config.oauthRedirectSignInLocal : config.oauthRedirectSignInPublic,
+    //       redirectSignOut: isLocalhost ? config.oauthRedirectSignOutLocal : config.oauthRedirectSignOutPublic,
+    //     },
+    //     region: process.env.REACT_APP_REGION,
+    //     userPoolId: process.env.REACT_APP_USER_POOL_ID,
+    //     userPoolWebClientId: process.env.REACT_APP_USER_POOL_WEB_CLIENT_ID,
+    //     storage: localStorage,
+    //   },
+    // });
 
     /**
     * TODO: I18n.setLanguage should enable amplify localized error messages,
